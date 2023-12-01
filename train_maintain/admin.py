885e08dfc357a5e066ee2010e3c2ce9d1ed94c5a
KaoruSawade <carlosoarde@gmail.com>
2023-12-02 01:23:11 +0800

    Edited train_maintain admin.py and views.py

--- train_maintain/admin.py
+++ train_maintain/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Train, MaintenanceCrew, MaintenanceTask
 
-# Register your models here.
+class TrainAdmin(admin.ModelAdmin):
+  model = Train
+
+class MaintenanceCrewAdmin(admin.ModelAdmin):
+  model = MaintenanceCrew
+
+class MaintenanceTaskAdmin(admin.ModelAdmin):
+  model = MaintenanceTask
+
+admin.site.register(Train, TrainAdmin)
+admin.site.register(MaintenanceCrew, MaintenanceCrewAdmin)
+admin.site.register(MaintenanceTask, MaintenanceTaskAdmin)
+
