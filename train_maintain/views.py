bf9ef3e3a26658e14190d604cb624a36704b0b87
KaoruSawade <carlosoarde@gmail.com>
2023-12-04 02:36:23 +0800

    Edited views.py to use raw queries, updated maintenance.html to show the records on a table, and deleted unused maintenance-detail.html

--- train_maintain/views.py
+++ train_maintain/views.py
@@ -1,16 +1,17 @@
-from .models import Train, MaintenanceTask, MaintenanceCrew
+from .models import Train as train, MaintenanceTask as maintenancetask, MaintenanceCrew as maintenancecrew
 from django.views.generic import DetailView
 from django.shortcuts import render
 from django.urls import reverse
 
 def pageview(request):
-    model_object = MaintenanceTask.objects.order_by("-date_maintained")
+    model_object = maintenancetask.objects.raw(
+        '''
+        SELECT maintenance_task_id, train_maintain_maintenancecrew.crew_id AS "Crew No.", train_maintain_maintenancecrew.crew_leader AS "Crew Leader", CONCAT('I.D: ', train_maintain_train.train_id,' (Model: ', train_maintain_train.train_model, ')') AS "Train Maintained", train_maintain_maintenancetask.date_maintained AS "Maintenance Date", train_maintain_maintenancetask.maintenance_description AS "Description of Maintenance", train_maintain_maintenancetask.condition AS "Conditions After Maintenance"
+        FROM train_maintain_maintenancecrew, train_maintain_train, train_maintain_maintenancetask
+        WHERE train_maintain_maintenancecrew.crew_id = train_maintain_maintenancetask.crew_id
+        AND train_maintain_train.train_id = train_maintain_maintenancetask.train_id
+        AND train_maintain_maintenancetask.date_maintained = '2023-08-14'
+        ORDER BY "Maintenance Date" DESC;
+        '''
+    )
     return render(request, "train_maintain/maintenance.html", context={"maintenance_list":model_object})
-
-class MaintainDetailView(DetailView):
-    model = MaintenanceTask
-    template_name = "train_maintain/maintenance-detail.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
