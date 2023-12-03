e94f1c8d05aed5f28b6202b555bfca7bad579437
KaoruSawade <carlosoarde@gmail.com>
2023-12-04 02:32:48 +0800

    Updated urls.py to remove ambiguity of urls

--- tirian_trains/urls.py
+++ tirian_trains/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('train_maintain.urls')),
     path('admin/', admin.site.urls),
     path('train_maintain/', include('train_maintain.urls', namespace="train_maintain")),
 ]
