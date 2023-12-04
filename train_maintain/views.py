8bcac339d4c874cb139300b7aa603b930eb5a798
KaoruSawade <carlosoarde@gmail.com>
2023-12-04 21:09:27 +0800

    Implemented ascending and descending order for columns and made the train maintained column sort by train_id instead of train_model

--- train_maintain/views.py
+++ train_maintain/views.py
@@ -4,13 +4,14 @@ from django.urls import reverse
 
 def pageview(request):
     # Get the parameter from the URL if any header is clicked
-    order_by = request.GET.get('order_by', 'date_maintained')  # Default sorting by 'date_maintained'
+    order_by = request.GET.get('order_by', 'date_maintained')  # Default to sorting by 'date_maintained'
+    arrangement = request.GET.get ('arrangement', '') # Default to descending
     
     # Define the columns that are sortable
     sortable_columns = {
         'maintenance_task_id': 'maintenance_task_id',
         'date_maintained': 'train_maintain_maintenancetask.date_maintained',
-        'train_model': 'train_maintain_train.train_model',
+        'train_id': 'train_maintain_train.train_id',
         'maintenance_description': 'train_maintain_maintenancetask.maintenance_description',
         'condition': 'train_maintain_maintenancetask.condition',
         'crew_id': 'train_maintain_maintenancecrew.crew_id',
@@ -19,7 +20,7 @@ def pageview(request):
     
     # Check if the column is sortable
     if order_by not in sortable_columns:
-        order_by = 'date_maintained'  # Set default sorting if the column is not in the sortable_columns
+        order_by = 'date_maintained'  # Set to default sorting if the column is not in the sortable_columns
 
     query = f'''
         SELECT maintenance_task_id, train_maintain_maintenancecrew.crew_id AS "Crew No.", 
@@ -31,10 +32,10 @@ def pageview(request):
         FROM train_maintain_maintenancecrew, train_maintain_train, train_maintain_maintenancetask
         WHERE train_maintain_maintenancecrew.crew_id = train_maintain_maintenancetask.crew_id
         AND train_maintain_train.train_id = train_maintain_maintenancetask.train_id
-        ORDER BY {sortable_columns[order_by]} DESC;
+        ORDER BY {sortable_columns[order_by]} {arrangement};
     '''
 
     # Fetch data using the raw SQL query
     model_object = maintenancetask.objects.raw(query)
 
-    return render(request, "train_maintain/maintenance.html", context={"maintenance_list":model_object})
+    return render(request, "train_maintain/maintenance.html", context={"maintenance_list":model_object, "order_by": sortable_columns[order_by], "arrangement": arrangement})
